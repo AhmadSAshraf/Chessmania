d146d7fb084cb009fea85f5fccdf406c9814f261
AhmadSAshraf <ahmadashraf159@gmail.com>
2022-12-29 22:05:28 +0300

    This commit will define private routes and 404 errors

---
+++ frontend/src/pages/BasePage.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import NotFound from '../components/NotFound';
+
+/**
+ * Base page used for all private routes and 404 errors.
+ */
+const BasePage = ({ main }) => {
+
+}
