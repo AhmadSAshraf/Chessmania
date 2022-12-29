0127eb8f4af1c1149cefb7e30847be77d2757b43
AhmadSAshraf <ahmadashraf159@gmail.com>
2022-12-29 22:32:48 +0300

    Fixed syntax error in base page

--- frontend/src/pages/BasePage.js
+++ frontend/src/pages/BasePage.js
@@ -3,74 +3,75 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import NotFound from '../components/NotFound';
 
+
 /**
  * Base page used for all private routes and 404 errors.
  */
 const BasePage = ({ main }) => {
-    const [mainClass, setmainClass] = useState(() => {
-        if ()
-            ow.innerWidth > 1400 {
+    const [mainClass, setMainClass] = useState(() => {
+        if (window.innerWidth > 1400) {
             return "large-main"
         }
         if (window.innerWidth > 940) {
             return "medium-main"
         }
-
         if (window.innerWidth > 580) {
             return "small-main"
         }
         return "compact-main"
     })
 
-
-}const []footerclass, setfooterClass = useStatee()() => {
-    if (window.innerWidth > 1400) {
-        return "large-main"
-    }
-    if (window.innerWidth > 940) {
-        return "medium-main"
-    }
-
-    if (window.innerWidth > 580) {
-        return "small-main"
-    }
-    return "compact-main"
-
-}
-
-useEffect(() =) {
-    const checkWindowWidth = () => {
+    const [footerClass, setFooterClass] = useState(() => {
         if (window.innerWidth > 1400) {
-            setmainClass("large-main")
-            setfooterClass("large-footer")
-        } else if (window.innerWidth > 940) {
-            setmainClass("medium-main")
-            setfooterClass("medium-footer")
-        } else if (window.innerWidth > 580) {
-            setmainClass("small-main")
-            setfooterClass("small-footer")
-        } else {
-            setmainClass("compact-main")
-            setfooterClass("compact-footer")
-
+            return "large-footer"
         }
+        if (window.innerWidth > 940) {
+            return "medium-footer"
+        }
+        if (window.innerWidth > 580) {
+            return "small-footer"
+        }
+        return "compact-footer"
+    })
+
+    useEffect(() => {
+        const checkWindowWidth = () => {
+            if (window.innerWidth > 1400) {
+                setMainClass("large-main")
+                setFooterClass("large-footer")
+            } else if (window.innerWidth > 940) {
+                setMainClass("medium-main")
+                setFooterClass("medium-footer")
+            }
+            else if (window.innerWidth > 580) {
+                setMainClass("small-main")
+                setFooterClass("small-footer")
+            } else {
+                setMainClass("compact-main")
+                setFooterClass("compact-footer")
+            }
+        }
+
         window.addEventListener("resize", checkWindowWidth)
+
         return () => {
             window.removeEventListener("resize", checkWindowWidth)
         }
-    }
-}, [] >
 
-     return (
-        < <>
-        <Header />
-        <main className={mainClass}>
-            {main ? main : <NotFound />}
-        </main>
-        <Footer
-            footerClass={footerClass}
-        />
-    </> </>
-     )
+    }, [])
+
+    return (
+        <>
+            <Header />
+            <main className={mainClass}>
+                {main ? main : <NotFound />}
+            </main>
+            <Footer
+                footerClass={footerClass}
+            />
+        </>
+    )
+}
 
 export default BasePage
+ePage
